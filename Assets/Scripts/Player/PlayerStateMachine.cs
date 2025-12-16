a93b72d7478517626081aa01afcc7f0b80da7619
majorsloth13 <samgottsch@gmail.com>
2025-12-16 16:15:59 -0700

    uiolh

--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -39,15 +39,37 @@ public class PlayerStateMachine : MonoBehaviour
 
     [Header("Power-Up")]
     public bool powerUpActive = true; // set true when player picks up power
-    public float powerUpTimer = 3f;
+    public float powerUpTimer;
     public bool hasActivated = false;
     // adjustable cooldown
-    public float gunCooldown = 0.25f;     
-   [HideInInspector] public float gunCooldownTimer = 0f;
+    public float gunCooldown = 0.25f;
+    [HideInInspector] public float gunCooldownTimer = 0f;
     public int maxDashCharges = 2;
     public int currentDashCharges = 2;
     public float dashRechargeTime = 3f;
     private float dashRechargeTimer = 0f;
+
+    public BoxCollider2D slashHitbox;
+
+    [Header("Power-Up Slots")]
+    public ScannedCard[] powerUpSlots = new ScannedCard[2]
+{
+    ScannedCard.None,
+    ScannedCard.None
+};
+
+    [Header("card tracking")]
+    public ToggleOnTracking AcetoggleTracking;
+    public ToggleOnTracking kingtoggleTracking;
+    public ToggleOnTracking queentoggleTracking;
+    public ToggleOnTracking jackleTracking;
+    public ToggleOnTracking jokertoggleTracking;
+
+    [Header("Diamond Skin")]
+    public bool diamondSkinActive = false;
+    public int diamondSkinMaxHealth = 6;
+    public int diamondSkinCurrentHealth = 1;
+    public GameObject diamondSkin;
 
     [Header("Drop Through Platform")]
     [SerializeField] private float dropDuration = 0.25f;
@@ -58,7 +80,6 @@ public class PlayerStateMachine : MonoBehaviour
     [Tooltip("The sound effect plays when player dies")]
     public AudioClip DeathSoundClip;
 
-    // runtime timers
     internal float coyoteTimer = 0f;
     internal float jumpBufferTimer = 0f;
 
@@ -78,7 +99,7 @@ public class PlayerStateMachine : MonoBehaviour
 
 
     // Helpers (exposed for states)
-    public bool IsGrounded => 
+    public bool IsGrounded =>
         !isDropping && GroundCheck != null && GroundCheck.IsGrounded();
     public bool IsTouchingWall => WallCheck != null && WallCheck.IsTouchingWall;
     public bool IsTouchingLeftWall => WallCheck != null && WallCheck.IsTouchingLeftWall;
@@ -102,55 +123,76 @@ public class PlayerStateMachine : MonoBehaviour
         SwitchState(new GroundedState(this)); // start inside grounded parent
     }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!IsGrounded)
         {
-            if (!IsGrounded)
-            {
-            
-                coyoteTimer -= Time.deltaTime;
-            }
-            else
-            {
-                coyoteTimer = coyoteTime; // rest whenever grounded
-            }
 
-            if (jumpBufferTimer > 0f)
-            {
-                jumpBufferTimer -= Time.deltaTime;
-            }
-
-            // capture jump input (buffer)
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                jumpBufferTimer = jumpBufferTime;
-            }
-
-            // cooldown countdown
-            if (gunCooldownTimer > 0f)
-            {
-                gunCooldownTimer -= Time.deltaTime;
-            }
-            if (Input.GetKeyDown(KeyCode.LeftShift) && powerUpTimer == 3)
-            {
-                hasActivated = true;
-                gun.gameObject.SetActive(false);
-
-            }
-            else if (powerUpTimer <= 0f)
-            {
-                hasActivated = false;
-                gun.gameObject.SetActive(true);
-            
+            coyoteTimer -= Time.deltaTime;
         }
-            // Power-up dash activation
-            if (!hasActivated && powerUpTimer <= 0)
-            {
+        else
+        {
 
-                powerUpTimer = 3f;
+            coyoteTimer = coyoteTime; // rest whenever grounded
+        }
 
 
-            }
+        if (jumpBufferTimer > 0f)
+        {
+            jumpBufferTimer -= Time.deltaTime;
+        }
+
+        // capture jump input (buffer)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferTimer = jumpBufferTime;
+        }
+
+        // cooldown countdown
+        if (gunCooldownTimer > 0f)
+        {
+            gunCooldownTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivatePowerUp(powerUpSlots[0]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActivatePowerUp(powerUpSlots[1]);
+        }
+
+        if (powerUpTimer <= 0f)
+        {
+            hasActivated = false;
+            gun.gameObject.SetActive(true);
+            //AceScanned = false;
+
+        }
+
+        // Deactivate shield if health <= 0
+        if (diamondSkinCurrentHealth <= 0)
+        {
+            Debug.Log("diamond skin broken");
+            diamondSkinActive = false;
+            diamondSkin.gameObject.SetActive(false);
+
+        }
+
+
+
+        // Power-up dash activation
+        if (!hasActivated && powerUpTimer <= 0)
+        {
+
+            powerUpTimer = 15f;
+
+
+        }
         if (hasActivated)
         {
             powerUpTimer -= Time.deltaTime;
@@ -163,35 +205,31 @@ public class PlayerStateMachine : MonoBehaviour
                     currentDashCharges++;
                     dashRechargeTimer = 0f;
                 }
-                
+
             }
             if (Input.GetMouseButtonDown(1) && currentDashCharges > 0)
             {
                 Debug.Log("slashed");
                 currentDashCharges--;
                 SwitchState(new MovementSlashState(this));
-                
+
                 return;
             }
-         
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(SlashCoroutine());
+            }
+
+            IEnumerator SlashCoroutine()
+            {
+                slashHitbox.enabled = true;
+                yield return new WaitForSeconds(0.15f); // slash active time
+                slashHitbox.enabled = false;
+            }
+
+
         }
-        
-
-
-
-        //if (Input.GetMouseButtonDown(0) && gunCooldownTimer <= 0f)
-        //{
-        //    SwitchState(new GunAttackState(
-        //        this,
-        //        //gunFireRate,
-        //        gunCooldown,
-        //        bulletPrefab,
-        //        gunFirePoint,
-        //        gunTransform,
-        //        bulletLifetime
-        //    ));
-        //    return;
-        //}
 
         if (Input.GetMouseButtonDown(0) && gunCooldownTimer <= 0f && !hasActivated)
         {
@@ -202,10 +240,16 @@ public class PlayerStateMachine : MonoBehaviour
 
         FlipToGunDirection();
 
-            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 direction = (mouseWorld - gunTransform.position).normalized;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mouseWorld - gunTransform.position).normalized;
 
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        gunTransform.rotation = Quaternion.Euler(0f, 0f, angle);
+
 
         // Check if the player is flipped (facing left)
         if (transform.localScale.x < 0)
@@ -215,6 +259,7 @@ public class PlayerStateMachine : MonoBehaviour
         }
 
         gunTransform.rotation = Quaternion.Euler(0f, 0f, angle);
+
 
             currentState?.Update();
         }
@@ -261,24 +306,6 @@ public class PlayerStateMachine : MonoBehaviour
         SwitchState(new FallState(this));
     }
 
-
-
-    /*private void HandleInput()
-    {
-        // 1 = Idle, 2 = Vertical, 3 = Horizontal
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SwitchState(new IdleState(this));
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SwitchState(new VerticalState(this));
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            SwitchState(new HorizontalState(this));
-        }
-    }*/
 
     public void SwitchState(IPlayerState newState)
     {
@@ -381,4 +408,91 @@ public class PlayerStateMachine : MonoBehaviour
         Object.Destroy(b, bulletLifetime);
     }
 
+    public enum ScannedCard
+    {
+        None,
+        Ace,
+        King,
+        Queen,
+        Jack,
+        Joker
+    }
+
+    public ScannedCard scannedCard = ScannedCard.None;
+
+    public void AssignScannedCardToSlot(ScannedCard card)
+    {
+        // Ignore duplicates
+        if (powerUpSlots[0] == card || powerUpSlots[1] == card)
+            return;
+
+        if (powerUpSlots[0] == ScannedCard.None)
+        {
+            powerUpSlots[0] = card;
+            Debug.Log(card + " assigned to Slot 1");
+        }
+        else if (powerUpSlots[1] == ScannedCard.None)
+        {
+            powerUpSlots[1] = card;
+            Debug.Log(card + " assigned to Slot 2");
+        }
+    }
+
+    void ActivatePowerUp(ScannedCard card)
+    {
+        switch (card)
+        {
+            case ScannedCard.Ace:
+                ActivateAce();
+                break;
+
+            case ScannedCard.King:
+                ActivateKing();
+                break;
+
+            case ScannedCard.Queen:
+                ActivateQueen();
+                break;
+
+            case ScannedCard.Jack:
+                ActivateJack();
+                break;
+            
+            case ScannedCard.Joker:
+                ActivateJoker();
+                break;
+        }
+    }
+
+    void ActivateAce()
+    {
+        Debug.Log("Ace power activated");
+        hasActivated = true;
+        gun.gameObject.SetActive(false);
+    }
+
+    void ActivateKing()
+    {
+        Debug.Log("King power activated");
+        diamondSkinCurrentHealth = diamondSkinMaxHealth;
+        diamondSkinActive = true;
+        diamondSkin.SetActive(true);
+    }
+
+    void ActivateQueen()
+    {
+
+    }
+
+    void ActivateJack()
+    {
+
+    }
+
+    void ActivateJoker()
+    {
+
+    }
+
 }
+
